18f6d6be31247a5a3da4e8843d6ddab2557c1189
shengnan <pelevin1111@gmail.com>
2024-10-22 13:21:17 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex flex-col min-h-[100vh]">
+        
+        <div class="">
+            Header
+        </div>
+        
+        <div class="flex-1">
+            Main
+        </div>
+        
+        <div class="mt-auto">
+            Footer
+        </div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
