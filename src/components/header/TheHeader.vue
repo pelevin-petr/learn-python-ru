1eda72440535c39f7b284cb79c85f085db500096
shengnan <pelevin1111@gmail.com>
2024-10-24 13:15:04 +0300

    add account icon and change-theme icon, update menu

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
+import nightModeIcon from '../../assets/night-mode-icon.svg'
+import pythonIcon from '../../../public/python-icon.svg'
+import accountAvatarIcon from '../../assets/account-avatar-icon.svg'
 
 </script>
 
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="../../../public/python-icon.svg" class="h-[30px]" alt="Logo"/>
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">learn.python.ru</span>
+      <div class="hidden md:block md:absolute md:top-[15px] md:left-[10px]  ">
+        <button>
+          <img :src="nightModeIcon" alt="This img don't supported by your browser" class="w-[40px]"/>
+        </button>
+        <hr id="hr">
+      </div>
+      <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse md:ml-[50px]">
+        <img :src="pythonIcon" class="h-[30px]" alt="This img don't supported by your browser"/>
+        <span class="hidden lg:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">learn.python.ru</span>
+        <span class="lg:hidden self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Py.Ru</span>
       </a>
-      <div class="flex md:order-2">
+      <div class="flex md:order-2 mr-[50px]">
         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false"
                 class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -28,8 +38,8 @@
             <span class="sr-only">Search icon</span>
           </div>
           <input type="text" id="search-navbar"
-                 class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                 placeholder="Поиск по учебнику">
+                 class="block w-full p-2 ps-7 lg:ps-12 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                 placeholder=" Поиск по учебнику">
         </div>
         <button data-collapse-toggle="navbar-search" type="button"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -74,29 +84,31 @@
               Тесты
             </a>
           </li>
-          
+          <li class="relative">
+            <a href="#"
+               class="md:hidden block py-2 px-3 mb-1 md:mb-0 border border-gray-300 md:border-none text-gray-900 rounded hover:text-blue-700 hover:border-gray-500 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              Изменить тему
+            </a>
+            <img :src="nightModeIcon" alt="This img don't supported by your browser" class="absolute w-[30px] top-[8px] left-[130px]">
+          </li>
+        
         </ul>
       </div>
+      <img :src="accountAvatarIcon" alt="This img don't supported by your browser" class="absolute w-[30px] right-[18px] top-[19px]">
     </div>
   </nav>
   <hr class="border-[2px]">
 </template>
 
 <style scoped lang="scss">
-.burger {
-  border: none;
-  background: transparent;
-  
-  &::before,
-  &::after {
-    content: '';
-    left: 0;
-    position: absolute;
-    display: block;
-    width: 100%;
-    height: 4px;
-    border-radius: 10px;
-    background: #000;
-  }
+#hr {
+  background-color: rgba(204, 204, 204, 0.58);
+  -webkit-transform: rotate(90deg);
+  position: absolute;
+  width: 107px;
+  height: 3px;
+  left: -5px;
+  top: 0;
+  border: 2px;
 }
 </style>
